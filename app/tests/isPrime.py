9fc0ce2740ada91f77b7a291f5e8c6c0faccd389
Jhonatan Martinez Muñoz <eljhona_@hotmail.com>
2023-05-04 15:06:42 -0500

    Prueba correcta

--- app/tests/isPrime.py
+++ app/tests/isPrime.py
@@ -1,3 +1,6 @@
 
-def is_prime(number):
-    return True
+def is_prime(n):
+  for i in range(2,n):
+    if (n%i) == 0:
+      return True
+  return False
--- app/tests/test.py
+++ app/tests/test.py
@@ -1,4 +1,4 @@
 from isPrime import is_prime
 
 def test_is_prime():
-    assert is_prime(3) == True
+    assert is_prime(2) == False
